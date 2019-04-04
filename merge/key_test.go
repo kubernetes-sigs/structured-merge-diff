a0c9bd9e025afd9c8803735af096e120a6017263
jennybuckley <jennybuckley@google.com>
2019-04-04 11:08:33 -0700

    Run gofmt

--- merge/key_test.go
+++ merge/key_test.go
@@ -1,29 +1,29 @@
 /*
 Copyright 2019 The Kubernetes Authors.
- 
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
- 
+
     http://www.apache.org/licenses/LICENSE-2.0
- 
+
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
- 
+
 package merge_test
- 
+
 import (
 	"testing"
- 
+
 	"sigs.k8s.io/structured-merge-diff/fieldpath"
 	. "sigs.k8s.io/structured-merge-diff/internal/fixture"
 	"sigs.k8s.io/structured-merge-diff/typed"
 )
- 
+
 var associativeListParser = func() typed.ParseableType {
 	parser, err := typed.NewParser(`types:
 - name: type
@@ -54,7 +54,7 @@ var associativeListParser = func() typed.ParseableType {
 	}
 	return parser.Type("type")
 }()
- 
+
 func TestUpdateAssociativeLists(t *testing.T) {
 	tests := map[string]TestCase{
 		"removing_obsolete_applied_structs": {
@@ -95,7 +95,7 @@ func TestUpdateAssociativeLists(t *testing.T) {
 			},
 		},
 	}
- 
+
 	for name, test := range tests {
 		t.Run(name, func(t *testing.T) {
 			if err := test.Test(associativeListParser); err != nil {
