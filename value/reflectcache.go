076ef8d6588e256b029f50d1ad5af51153b72654
Chris Tarazi <tarazichris@gmail.com>
2020-09-17 11:32:46 -0700

    Fix panic on pointer to embedded struct
    This commit converts an embedded struct into its original type if it is
    a pointer.
    
    Fixes:
    https://github.com/kubernetes-sigs/structured-merge-diff/issues/172
    
    Signed-off-by: Chris Tarazi <tarazichris@gmail.com>

--- value/reflectcache.go
+++ value/reflectcache.go
@@ -74,7 +74,7 @@ func (f *FieldCacheEntry) CanOmit(fieldVal reflect.Value) bool {
 func (f *FieldCacheEntry) GetFrom(structVal reflect.Value) reflect.Value {
 	// field might be nested within 'inline' structs
 	for _, elem := range f.fieldPath {
-		structVal = structVal.FieldByIndex(elem)
+		structVal = dereference(structVal).FieldByIndex(elem)
 	}
 	return structVal
 }
@@ -150,7 +150,11 @@ func buildStructCacheEntry(t reflect.Type, infos map[string]*FieldCacheEntry, fi
 			continue
 		}
 		if isInline {
-			buildStructCacheEntry(field.Type, infos, append(fieldPath, field.Index))
+			e := field.Type
+			if field.Type.Kind() == reflect.Ptr {
+				e = field.Type.Elem()
+			}
+			buildStructCacheEntry(e, infos, append(fieldPath, field.Index))
 			continue
 		}
 		info := &FieldCacheEntry{JsonName: jsonName, isOmitEmpty: isOmitempty, fieldPath: append(fieldPath, field.Index), fieldType: field.Type}
